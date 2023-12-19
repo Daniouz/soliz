88250f5deba5a1949b862c55e333b368deb4cac4
Izaah Birmingham <153025265+Daniouz@users.noreply.github.com>
2023-12-19 16:33:24 -0500

    Initial commit

---
+++ examples/json/src/main.py
@@ -0,0 +1,38 @@
+from typing import Tuple
+
+from soliz.error import Error
+from soliz.impls import StringRule, NumberRule
+from soliz.lex import Lexer, Rule, Context
+from soliz.tokens import Token
+
+
+class TokenType:
+    TT_LBRACKET = "LBRACKET"
+    TT_RBRACKET = "RBRACKET"
+    TT_COLON = "COLON"
+
+
+class JSONRules(Rule):
+    def check(self, ctx: Context) -> Tuple[Token, bool] | None:
+        match ctx.char:
+            case '{':
+                return Token(TokenType.TT_LBRACKET, ctx.span()), True
+            case '}':
+                return Token(TokenType.TT_RBRACKET, ctx.span()), True
+            case ':':
+                return Token(TokenType.TT_COLON, ctx.span()), True
+
+
+def main() -> None:
+    lexer = Lexer([JSONRules(), StringRule(), NumberRule()])
+    text = input("Enter JSON: ")
+
+    try:
+        tokens = lexer.lex(text)
+        print(tokens)
+    except Error as err:
+        err.pretty_print(text)
+
+
+if __name__ == "__main__":
+    main()
